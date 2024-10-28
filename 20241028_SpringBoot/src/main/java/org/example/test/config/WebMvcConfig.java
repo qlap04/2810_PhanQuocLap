c105ff2eb920f6bedbadad465ddbc218190b1333
qlap04 <uchihaquoc04@gmail.com>
2024-10-28 16:39:39 +0700

    second

--- 20241028_SpringBoot/src/main/java/org/example/test/config/WebMvcConfig.java
+++ 20241028_SpringBoot/src/main/java/org/example/test/config/WebMvcConfig.java
@@ -18,23 +18,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean(name = "localeResolver")
     public LocaleResolver getLocaleResolver() {
         CookieLocaleResolver resolver = new CookieLocaleResolver();
-        resolver.setCookieDomain("iotstar.vn");
-        resolver.setDefaultLocale(Locale.ENGLISH);
+        resolver.setDefaultLocale(Locale.ENGLISH); // Ngôn ngữ mặc định
+        resolver.setCookieMaxAge(3600); // Cookie tồn tại trong 1 giờ
         return resolver;
     }
 
     @Bean(name = "messageSource")
     public MessageSource getMessageResource() {
         ReloadableResourceBundleMessageSource messageResource = new ReloadableResourceBundleMessageSource();
-        messageResource.setBasename("classpath:i18n/messages");
-        messageResource.setDefaultEncoding("UTF-8");
+        messageResource.setBasename("classpath:i18n/messages"); // Đường dẫn tới các tệp message
+        messageResource.setDefaultEncoding("UTF-8"); // Đảm bảo UTF-8 để hiển thị tiếng Việt
         return messageResource;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        localeInterceptor.setParamName("language");
-        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
+        localeInterceptor.setParamName("language"); // Tham số URL để thay đổi ngôn ngữ
+        registry.addInterceptor(localeInterceptor).addPathPatterns("/**");
     }
 }
